8d0b207c273186919302fdce2ca0e9916de9e15a
OadeO6 <abdurrahman.adenowo06@gmail.com>
2024-03-08 01:25:22 +0100

    second commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    """
+    pascal triamgle
+    """
+    triangle = []
+    if n < 1:
+        return triangle
+    triangle.append([1])
+    for i in range(2, n+1):
+        new = []
+        for j in range(len(triangle[-1])):
+            if j == 0:
+                new.append(1)
+            else:
+                new.append(triangle[-1][j] + triangle[-1][j - 1])
+
+            if j == len(triangle[-1]) - 1:
+                new.append(1)
+        triangle.append(new)
+    return(triangle)
